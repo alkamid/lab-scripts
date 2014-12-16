b5cb6bbdbe8b55401028eb836da39fbd731ab83c
alkamid <adamkli@gmail.com>
2014-12-16 11:31:35 +0000

    easier to make custom changes

--- matplotlib/matplotLIV.py
+++ matplotlib/matplotLIV.py
@@ -18,7 +18,7 @@ class matplotLIV():
         self.title = title
         self.ylim = ylim
         
-        filenames = [("%s_%dK.txt" % (self.BaseFilename, temp), temp) for temp in self.temperatures]
+        filenames = [("%s_%sK.txt" % (self.BaseFilename, str(temp)), temp) for temp in self.temperatures]
         self.rawData = [(np.loadtxt(fname), temp) for fname, temp in filenames]
         self.colors = colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#e6ab02', '#a6761d', '#666666']
 
@@ -26,12 +26,15 @@ class matplotLIV():
 
     def plot(self):
         
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
+        self.fig = plt.figure()
+        self.ax1 = self.fig.add_subplot(111)
+        ax1 = self.ax1
         ax1.tick_params(bottom='off')
         ax1.xaxis.tick_top()
-        ax2 = ax1.twinx()
-        ax3 = ax2.twiny()
+        self.ax2 = ax1.twinx()
+        ax2 = self.ax2
+        self.ax3 = ax2.twiny()
+        ax3 = self.ax3
         ax3.xaxis.tick_bottom()
 
         ax1.set_xlabel("current / A")
@@ -43,8 +46,8 @@ class matplotLIV():
 
         for i, (datafile, label) in enumerate(self.rawData):
             self.checkMaxValues(datafile)
-            ax1.plot( datafile[:,0], datafile[:,1], color=self.colors[i], label='%dK' % label)
-            ax2.plot( datafile[:,0], datafile[:,2], color=self.colors[i], label='%dK' % label, linewidth=2)
+            ax1.plot( datafile[:,0], datafile[:,1], color=self.colors[i], label='%sK' % str(label))
+            ax2.plot( datafile[:,0], datafile[:,2], color=self.colors[i], label='%sK' % str(label), linewidth=2)
 
 
         ax1.margins(x=0)
@@ -61,15 +64,11 @@ class matplotLIV():
         ax3.set_xlim(start/self.area, end/self.area)
         ax2.legend(loc='upper left')
 
-        fig.suptitle(self.title, y=0.98, weight='bold')
-        fig.subplots_adjust(top=0.86)
+        self.fig.suptitle(self.title, y=0.98, weight='bold')
+        self.fig.subplots_adjust(top=0.86)
 
         loc = plticker.MultipleLocator(base=20.0) # this locator puts ticks at regular intervals
         ax3.xaxis.set_major_locator(loc)
-
-        plt.savefig(self.BaseFilename + '.pdf')
-
-        plt.show()
 
     def checkMaxValues(self, data):
         maxInd = data.argmax(axis=0)[2]
@@ -81,3 +80,7 @@ class matplotLIV():
         y1Fraction = self.maxValueRow[1]/yrange[1]
         y2Fraction = y1Fraction - 0.02
         ax2.set_ylim(top=self.maxValueRow[2]/y2Fraction)
+
+    def show(self):
+        plt.savefig(self.BaseFilename + '.pdf')        
+        plt.show()
